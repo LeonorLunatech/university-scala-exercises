88dd3bc8a163c1ffe8188ab478e23c3b7d82ebbc
LeonorLunatech <leonor.boga@lunatech.com>
2016-07-06 14:27:59 +0200

    Solution for 1-basics

--- chapter3-next-steps-in-scala/1-basics/exercise3.scala
+++ chapter3-next-steps-in-scala/1-basics/exercise3.scala
@@ -1,5 +1,12 @@
 // Define the method makeThreeTuple so that the following code compiles:
 
+def makeThreeTuple(a : String, b: String, c: String) : (String, String, String) ={
+  (a,b,c)
+}
+
+def makeThreeTuple(a : String, b: String, c: Double) : (String, String, Double) ={
+  (a,b,c)
+}
 
 // Write your answer above this line and don't change the code below this line!
 val allStrings = makeThreeTuple("foo", "bar", "buzzz")
