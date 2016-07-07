39c1e38fbe5b7dffc23a9c53a3983e8d7b634bad
LeonorLunatech <leonor.boga@lunatech.com>
2016-07-07 14:25:25 +0200

    Solutions

--- chapter3-next-steps-in-scala/3-collections/src/main/scala/com/lunatech/exercises/collections/Part01.scala
+++ chapter3-next-steps-in-scala/3-collections/src/main/scala/com/lunatech/exercises/collections/Part01.scala
@@ -21,45 +21,67 @@ object Part01 {
   /**
    * Exercise: Return the first five numbers, or all numbers if there are less than five
    */
-  def exercise2(i: List[Int]): List[Int] = ???
+  def exercise2(i: List[Int]): List[Int] =
+    i.take(5)
   
   /**
    * Exercise: Return the list without the first five numbers
    */
-  def exercise3(i: List[Int]): List[Int] = ???
+  def exercise3(i: List[Int]): List[Int] =
+    i.drop(5)
   
   /**
    * Exercise: Return the square of all numbers
    */
-  def exercise4(i: List[Int]): List[Int] = ???
+  def exercise4(i: List[Int]): List[Int] =
+    i.map { x => x * x}
   
   /**
    * Exercise: Return the product of all the numbers using a fold. Return 1 if the list is empty.
    */
-  def exercise5(i: List[Int]): Int = ???
+  def exercise5(i: List[Int]): Int = {
+    i.fold(1) { (x, y) => x * y
+    }
+  }
   
   /**
    * Exercise: Return the maximum of the parameter list,
    * and None when the parameter list is empty. 
-   * Hint: Use `foldLeft` 
-   */
-  def exercise6(input: List[Int]): Option[Int] = ???
+   * Hint: Use `foldLeft`
+    * */
+  def exercise6(input: List[Int]): Option[Int] = input match {
+    case Nil => None
+    case head :: tail => Some( tail.foldLeft(head)((r, c ) => (if( r > c) r else c )))
+  }
   
   /**
    * Exercise: Return a tuple, where the first element contains all the input elements >= 10
-   * and the second element contains all the input elements <= 10
+   * and the second element contains all the input elements < 10
    */
-  def exercise7(input: List[Int]): (List[Int], List[Int]) = ???
+  def exercise7(input: List[Int]): (List[Int], List[Int]) = {
+    input.partition(_ >= 10)
+  }
   
   /**
    * Exercise: Return a list of integers, where each input element is repeated it's own value times.
    * So input `List(1,3,2)` would give output `List(1,3,3,3,2,2)`. 
    * Hint: Use `List.fill` to create repeated lists as part of the solution. 
    */
-  def exercise8(input: List[Int]): List[Int] = ???
+  def exercise8(input: List[Int]): List[Int] = input.flatMap { x => List.fill(x)(x)}
   
   /**
    * Exercise: Return the even input elements squared, drop the other elements.
    */
-  def exercise9(input: List[Int]): List[Int] = ???
+  def exercise9(input: List[Int]): List[Int] = {
+
+    val evenSquare = new PartialFunction[Int, Int] {
+      def apply(x: Int) = x * x
+      def isDefinedAt(x: Int) = x % 2 == 0
+    }
+
+    input.collect(evenSquare)
+  }
 }
+
+
+
