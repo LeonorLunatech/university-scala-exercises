fd67369eb64f73a4328ff414cffe84485a74788a
LeonorLunatech <leonor.boga@lunatech.com>
2016-07-14 15:27:45 +0200

    exercise 4 solved

--- chapter4-classes-and-objects/src/main/scala/com/lunatech/Hypotenuse.scala
+++ chapter4-classes-and-objects/src/main/scala/com/lunatech/Hypotenuse.scala
@@ -1,8 +1,40 @@
+/*
+ * Copyright 2016 leonorboga
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.lunatech
 
-/**
-  * Created by leonorboga on 13/07/16.
-  */
-object Hyotenuse extends App {
+import scala.util.Try
 
+object Hypotenuse extends App {
+  if (args.length != 2) {
+    println("Usage: run <Integer> <Integer>")
+  } else {
+    val a = Try(Integer.parseInt(args(0)))
+    val b = Try(Integer.parseInt(args(1)))
+
+    if (a.isSuccess && b.isSuccess) {
+      val hypotenuse = calculateHypotenuse(a.get, b.get)
+      println(hypotenuse)
+    } else
+      println("Numbers only please!")
+
+  }
+
+  def calculateHypotenuse(a: Int, b: Int): Int = {
+    Math.sqrt((a * a) + (b * b)).toInt
+  }
 }
+
